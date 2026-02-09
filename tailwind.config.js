4214fd7993164ca4dd185533b320b192a14e11b9
vlongle29 <longvis2004@gmail.com>
2026-02-10 02:46:01 +0700

    config tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,47 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+   theme: {
+      extend: {
+         colors: {
+            primary: "#f84565",
+            secondary: "#09090b",
+            "dark-bg": "#09090b",
+            "card-bg": "#12161c",
+         },
+         textColor: {
+            primary: "#ffffff",
+            secondary: "#d1d5dc",
+            muted: "#797b7d",
+         },
+         fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+            heebo: ["Heebo", "sans-serif"],
+         },
+         spacing: {
+            7.5: "1.875rem",
+            17.5: "4.375rem",
+            3.75: "15px",
+         },
+         gap: {
+            3.75: "15px",
+         },
+         maxWidth: {
+            common: "1200px",
+         },
+         height: {
+            785: "785px",
+            500: "500px",
+            400: "400px",
+            280: "280px",
+            250: "250px",
+            200: "200px",
+            140: "140px",
+         },
+         width: {
+            173: "173px",
+         },
+      },
+   },
+   plugins: [],
+};
